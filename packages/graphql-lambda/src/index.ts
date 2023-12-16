5542b8f49c851f7302849f3ed3b72f3d89987dfe
Matthew Benton <m@mattb.tech>
2023-12-16 13:11:29 +0000

    Bootstrap graphql

---
+++ packages/graphql-lambda/src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
+import { Resolvers } from "./generated/graphql";
+import { buildSubgraphSchema } from "@apollo/subgraph";
+import gql from "graphql-tag";
+
+const typeDefs = gql`
+  extend schema
+    @link(
+      url: "https://specs.apollo.dev/federation/v2.0"
+      import: ["@key", "@shareable"]
+    )
+
+  extend type Query {
+    hello: String!
+  }
+`;
+
+const resolvers: Resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({
+    typeDefs,
+    resolvers,
+  }),
+});
+
+export const handler = startServerAndCreateLambdaHandler(server);
