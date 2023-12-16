299eef94d8897c3a3596347191c8edd7fa34f284
Matthew Benton <m@mattb.tech>
2023-12-16 19:31:54 +0000

    Basic vending of video URLs

--- packages/graphql-lambda/src/index.ts
+++ packages/graphql-lambda/src/index.ts
@@ -6,6 +6,16 @@ import {
 import { Resolvers } from "./generated/graphql";
 import { buildSubgraphSchema } from "@apollo/subgraph";
 import gql from "graphql-tag";
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import { cleanEnv, str } from "envalid";
+
+const { BUCKET } = cleanEnv(process.env, {
+  BUCKET: str(),
+});
+
+const CDN_DOMAIN = "https://recordings.medio.mattb.tech";
+
+const S3 = new S3Client({});
 
 const typeDefs = gql`
   extend schema
@@ -15,13 +25,26 @@ const typeDefs = gql`
     )
 
   extend type Query {
-    hello: String!
+    recordings: [Recording!]!
+  }
+
+  type Recording {
+    url: String!
   }
 `;
 
 const resolvers: Resolvers = {
   Query: {
-    hello: () => "world",
+    recordings: async () => {
+      const objectsList = await S3.send(
+        new ListObjectsV2Command({ Bucket: BUCKET }),
+      );
+      return (
+        objectsList.Contents?.map((object) => ({
+          url: `${CDN_DOMAIN}/${object.Key}`,
+        })) ?? []
+      );
+    },
   },
 };
 
